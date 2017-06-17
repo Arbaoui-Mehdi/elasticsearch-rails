e9318233a1ac88af4a2d1fbd2bcc00b11db27779
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-06-17 16:50:37 -0700

    Autosuggest

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
+
   resources :articles
   get "search", to: "search#search"
+  get 'search/typeahead/:term' => 'search#typeahead'
 end
